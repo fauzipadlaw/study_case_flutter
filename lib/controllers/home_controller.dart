a7e3a349275aba59b88e2b88968e530afc4b954b
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2022-06-25 20:21:11 +0700

    function to update filter paramters

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -4,41 +4,55 @@ import 'package:study_case_flutter/models/category.dart';
 import 'package:study_case_flutter/providers/home_provider.dart';
 
 class HomeController extends GetxController {
-  var filter = {"size": 12, "page": 1, "listCategory": []}.obs;
+  var size = 12.obs;
+  var page = 1.obs;
+  var listCategory = [].obs;
+  var businessName = "".obs;
   var businessList = [].obs;
   var categories = [].obs;
   HomeProvider homeProvider = Get.put(HomeProvider());
 
   @override
   void onInit() {
-    fetchBusiness(filter);
-    fetchCategories();
+    fetchBusiness(buildFilter());
     super.onInit();
+  }
+
+  void updatePage(int s) => size.value = s;
+  void updateBusinessName(String s) => businessName.value = s;
+  void updateCategory(List<String> categories) =>
+      listCategory.value = categories;
+
+  Map buildFilter() {
+    var filter = {"size": size.value, "page": page.value, "listCategory": listCategory.value};
+    if (businessName.value.isNotEmpty) filter["businessName"] = businessName.value;
+    return filter;
   }
 
   void fetchBusiness(Map filter) {
     homeProvider.listBusiness(filter).then((response) {
-      print("response ${response.statusCode}");
-      if(response.statusCode == 200) {
-        businessList.value = List<Business>.from(response.body["data"]["content"]
+      if (response.statusCode == 200) {
+        businessList.value = List<Business>.from(response.body["data"]
+                ["content"]
             .map((model) => Business.fromJson(model)));
       } else {
-        Get.snackbar("Error ${response.statusCode}", "Galat mendapat daftar bisnis. ${response.statusCode}", snackPosition: SnackPosition.BOTTOM);
+        Get.snackbar("Error ${response.statusCode}",
+            "Galat mendapat daftar bisnis. ${response.statusCode}",
+            snackPosition: SnackPosition.BOTTOM);
       }
-
     });
   }
 
   void fetchCategories() {
     homeProvider.listCategories().then((response) {
-      print("response ${response.statusCode}");
-      if(response.statusCode == 200) {
+      if (response.statusCode == 200) {
         categories.value = List<Category>.from(
             response.body["data"].map((model) => Category.fromJson(model)));
       } else {
-        Get.snackbar("Error ${response.statusCode}", "Galat mendapat daftar kategori bisnis. Silakan Coba lagi.", snackPosition: SnackPosition.BOTTOM);
+        Get.snackbar("Error ${response.statusCode}",
+            "Galat mendapat daftar kategori bisnis. Silakan Coba lagi.",
+            snackPosition: SnackPosition.BOTTOM);
       }
-
     });
   }
 }
